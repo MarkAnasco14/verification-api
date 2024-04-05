0fa9f07c9caedf224715afdcaecddf0ede6ed601
Mark <markanasco55@gmail.com>
2024-04-05 14:05:38 +0800

    first commit

---
+++ accounts/refresh-token.model.js
@@ -0,0 +1,71 @@
+
+// const { DataTypes } = require("sequelize");
+
+
+
+module.exports = model;
+
+
+
+function model(sequelize){
+    const attributes = {
+      token: { type: DataTypes.STRING },
+      expires: { type: DataTypes.DATE },
+      created: { type: DataTypes.DATE },
+      createdByIp: { type: DataTypes.STRING },
+      revoked: { type: DataTypes.DATE},
+      revokedById: { type: DataTypes.STRING},
+      replacedByToken: { type: DataTypes.STRING},
+      isExpired: {
+        type: DataTypes.VIRTUAL,
+        get() { return Date.now () >= this.expires; }
+      },
+     isActive: {
+        type: DataTypes.VIRTUAL,
+        get() { return !this.revoked && !this.isExpired; }
+     }
+    };
+
+    const options = {
+        // disable default timestamp fields ( created and updated)
+        timestamps: false
+    };
+    return sequelize.define('refreshToken', attributes, options);
+}const { DataTypes } = require("sequelize");
+
+module.exports = model;
+
+function model(sequelize) {
+  const attributes = {
+    token: { type: DataTypes.STRING },
+    expires: { type: DataTypes.DATE },
+    created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    createdByIp: { type: DataTypes.STRING },
+    revoked: { type: DataTypes.DATE },
+    revokedByIp: { type: DataTypes.STRING },
+    replacedByToken: { type: DataTypes.STRING },
+    isExpired: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return Date, now() >= this.expires;
+      },
+    },
+
+    isActive: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return !this.revoked && !this.isExpired;
+      },
+    },
+  };
+
+  const options = {
+    timestamps: false,
+  };
+
+  return sequelize.define("refreshToken", attributes, options);
+}
